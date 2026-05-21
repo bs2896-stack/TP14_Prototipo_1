ca616d62c65fc5b5bb9944dba63aefd924b9efef
bs2896-stack <bs2896@est.ort.edu.ar>
2026-05-21 11:53:20 -0300

    Clase 3: Implementación de GameManager y UIManager con TextMeshPro

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    public TextMeshProUGUI txtScore;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateScore(0);
+    }
+
+    public void UpdateScore(int score){
+        
+        txtScore.text = score.ToString();
+
+    }
+}
